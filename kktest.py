7f53965e69d5a3609fd196008037753bd5825686
Kirill Kresling <kirillkresling@MLA0RLI091.local>
2017-06-27 16:42:39 -0400

    Second Commit

--- kktest.py
+++ kktest.py
@@ -1,19 +1,18 @@
 #!/usr/local/bin/python
 
 def getInput():
-	inputtext = input("Would you like to run? (y/n):  ")
+	inputtext = input("Would you like to run? (yes/no):  ")
 	giveResponse(inputtext)
 
 def giveResponse(inputtext):
-	if (str(inputtext) == "y"):
+	if (str(inputtext) == "y" or "yes"):
 		print("Hello World")
-	elif (str(inputtext) == "n"):
+	elif (str(inputtext) == "n" or "no"):
 			print("Bye World")
 	else: 
 			print("Not a correct input") 
 			return getInput
 def main():
-	print("its working")
 	getInput()
 
 if __name__=='__main__':
